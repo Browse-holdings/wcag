1c5fc12bb2fc6be0e0302e4225c5445ecc8f5425
Michael Cooper <cooper@w3.org>
2020-05-29 12:40:21 -0400

    config to publish today

--- conformance-challenges/respec-config.js
+++ conformance-challenges/respec-config.js
@@ -19,15 +19,15 @@ var respecConfig = {
 	
 	
 	// if you wish the publication date to be other than today, set this
-	//publishDate:  "2014-12-11",
+	publishDate:  "2020-05-29",
 	copyrightStart:  "2020",
 	license: "w3c-software-doc",
 	noRecTrack: true,
 	
 	// if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
 	// and its maturity status
-	//previousPublishDate:  "2014-06-12",
-	//previousMaturity:  "WD",
+	previousPublishDate:  "2020-05-01",
+	previousMaturity:  "FPWD",
 	//prevRecURI: "https://www.w3.org/TR/2018/REC-WCAG21-20180605/",
 	//previousDiffURI: "https://www.w3.org/TR/2014/REC-wai-aria-20140320/",
 	
